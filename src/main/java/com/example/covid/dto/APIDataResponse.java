658634afa60e10f3ae9323c3826f1c78220c0e26
Ezra Lim <skywno@gmail.com>
2022-09-19 14:19:54 +0300

    Update #1 - APIDataResponse
    * `Object` -> 제네릭으로 변경하여 type-safety 향상
    * 이 표준 응답은 성공 응답이므로, 불필요한 파라미터 정리
    * 실패, 에러 응답은 `APIErrorResponse` 사용할 것
    * `empty()` 추가

--- src/main/java/com/example/covid/dto/APIDataResponse.java
+++ src/main/java/com/example/covid/dto/APIDataResponse.java
@@ -1,6 +1,7 @@
 package com.example.covid.dto;
 
 
+import com.example.covid.constant.ErrorCode;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,16 +9,20 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode(callSuper = true)
-public class APIDataResponse extends com.example.covid.dto.APIErrorResponse {
+public class APIDataResponse<T> extends com.example.covid.dto.APIErrorResponse {
 
-    private final Object data;
+    private final T data;
 
-    private APIDataResponse(boolean success, Integer errorCode, String message, Object data) {
-        super(success, errorCode, message);
+    private APIDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
         this.data = data;
     }
 
-    public static APIDataResponse of (boolean success, Integer errorCode,String message, Object data) {
-        return new APIDataResponse(success, errorCode, message, data);
+    public static <T> APIDataResponse<T> of (T data) {
+        return new APIDataResponse(data);
+    }
+
+    public static <T> APIDataResponse<T> empty() {
+        return new APIDataResponse<>(null);
     }
 }
