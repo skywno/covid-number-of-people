ce628d449f25db7248a8089ce2501c06f2aa28e3
Ezra Lim <skywno@gmail.com>
2022-09-28 11:18:04 +0300

    Add #5 - repository interfaces
    각 도메인에 연결할 리포지토리 인터페이스 작성
    `JpaRepository`를 기본으로 하고
    `Event`는 수업에 사용할 목적이므로 필요한 기능을 좀 더 구현

--- src/main/java/com/example/covid/repository/EventRepository.java
+++ src/main/java/com/example/covid/repository/EventRepository.java
@@ -1,24 +1,29 @@
 package com.example.covid.repository;
 
-import com.example.covid.constant.EventStatus;
-import com.example.covid.dto.EventDTO;
+import com.example.covid.domain.Event;
+import com.example.covid.domain.QEvent;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+public interface EventRepository extends
+        JpaRepository<Event, Long>,
+        QuerydslPredicateExecutor<Event>,
+        QuerydslBinderCustomizer<QEvent>
+{
 
-public interface EventRepository {
-    default List<EventDTO> findEvents(
-            Long locationId,
-            String eventName,
-            EventStatus eventStatus,
-            LocalDateTime eventStartDateTime,
-            LocalDateTime eventEndDateTime
-    ){
-        return null;
+    @Override
+    default void customize(QuerydslBindings bindings, QEvent root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.locationId, root.name, root.status,
+                root.eventStartDateTime, root.eventEndDateTime);
+        bindings.bind(root.name).first(StringExpression::likeIgnoreCase);
+        bindings.bind(root.eventStartDateTime).first(ComparableExpression::goe);
+        bindings.bind(root.eventEndDateTime).first(ComparableExpression::loe);
     }
-    default Optional<EventDTO> findEvent(Long eventId) { return Optional.empty(); }
-    default boolean insertEvent(EventDTO eventDTO) { return false; }
-    default boolean updateEvent(Long eventId, EventDTO dto) { return false; }
-    default boolean deleteEvent(Long eventId) { return false; }
+
+
 }
