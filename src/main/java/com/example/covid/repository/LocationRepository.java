f5301b2a012a14967f60d79b8514397248b4833a
Ezra Lim <skywno@gmail.com>
2022-10-02 15:57:08 +0300

    #7 - Implement LocationRepository

--- src/main/java/com/example/covid/repository/LocationRepository.java
+++ src/main/java/com/example/covid/repository/LocationRepository.java
@@ -1,7 +1,26 @@
 package com.example.covid.repository;
 
 import com.example.covid.domain.Location;
+import com.example.covid.domain.QLocation;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends
+        JpaRepository<Location, Long>,
+        QuerydslPredicateExecutor<Location>,
+        QuerydslBinderCustomizer<QLocation>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QLocation root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.locationName, root.address,root.phoneNumber);
+        bindings.bind(root.locationName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.address).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.phoneNumber).first(StringExpression::containsIgnoreCase);
+    }
+
 }
