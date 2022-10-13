051d884b53a4f04bf9d011f5c1148478f2707a43
Ezra Lim <skywno@gmail.com>
2022-10-13 15:06:30 +0300

    Update #17 - LocationResponse to include `id`

--- src/main/java/com/example/covid/controller/error/BaseErrorController.java
+++ src/main/java/com/example/covid/controller/error/BaseErrorController.java
@@ -3,6 +3,7 @@ package com.example.covid.controller.error;
 import com.example.covid.constant.ErrorCode;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
--- src/main/java/com/example/covid/dto/LocationResponse.java
+++ src/main/java/com/example/covid/dto/LocationResponse.java
@@ -3,6 +3,7 @@ package com.example.covid.dto;
 import com.example.covid.constant.LocationType;
 
 public record LocationResponse(
+        Long id,
         LocationType locationType,
         String locationName,
         String address,
@@ -10,17 +11,19 @@ public record LocationResponse(
         Integer capacity
 ) {
     public static LocationResponse of(
+            Long id,
             LocationType locationType,
             String locationName,
             String address,
             String phoneNumber,
             Integer capacity
     ) {
-        return new LocationResponse(locationType, locationName, address, phoneNumber, capacity);
+        return new LocationResponse(id, locationType, locationName, address, phoneNumber, capacity);
     }
 
     public static LocationResponse from (LocationDto dto) {
         return LocationResponse.of(
+                dto.id(),
                 dto.locationType(),
                 dto.locationName(),
                 dto.address(),
